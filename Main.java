ae3c20688e3756d6150b0f17e0cee24bdc97ca52
Rozalia <67601688+RozaliaM@users.noreply.github.com>
2020-06-30 15:51:45 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,85 @@
+import sun.font.TrueTypeFont;
+import sun.security.mscapi.CPublicKey;
+
+public class Main {
+
+    // public static void main (String[] args) {
+    //System.out.print("Hello World!!");    }
+
+    // задание 1
+
+    //  public static void main(String[] args) {
+    //  }
+//}
+
+    // задание 2
+    public static void main(String[] arg) {
+        //  byte a = 1;
+        //  System.out.println(a);
+        // short b = 2;
+        // System.out.println(b);
+        // int c = 3;
+        // System.out.println(c);
+        //  long d = 4L;
+        //  System.out.println(d);
+        // float f = 5f;
+        // System.out.println(f);
+        //  double g = 5.55;
+        //  System.out.println(g);
+        // char y = 'B';
+        // System.out.println(y);
+        // boolean o = true;
+        // System.out.println(o);
+        //  }
+//}
+        // задание 3
+            int a=1;
+            int b=2;
+            int c=4;
+            int d=2;
+         System.out.println(a=a*(b+(c/d)));
+    }
+}
+        // задание 4
+        // int a=10;
+        //  int b=15;
+        //  int c = a + b;
+        //  if (c >= 10 && c <= 20) {
+        //     System.out.println(true);
+        //  } else {
+        //      System.out.print(false);
+        //  }
+        //  }
+
+        // задание 5
+        // int a=6;
+        //if (a>0) {
+        //     System.out.println("Число положительное");
+// } else {
+        //       System.out.println("Число отрицательное");
+        //    }
+        // }
+        //  }
+
+        // задание 6
+// int a=6;
+//  if (a<0);
+        //      {
+        //          System.out.println(true);
+        //      }
+//    }
+//    }
+
+        // задание 7
+// String s= "Розалия";
+        // System.out.println("Привет,"+s+"!");
+        //  }
+//}
+
+        // задание 8
+ //       int a = 0;
+  //      if (a == 4 && a == 400) ;{
+  //          System.out.println("Год високосный");
+  //      }
+  //     }
+  //  }
